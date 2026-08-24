584c71e19ea3c34482ea3d7261fe261f140fcd47
Thanakrit Intui <138757081+thanakrit41652@users.noreply.github.com>
2026-08-24 11:04:35 +0700

    add lab2

---
+++ Lab02/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Student ID : 1690700784
+ * Name       : ธนกฤต อินทุย
+ * Section    : 129 A
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
