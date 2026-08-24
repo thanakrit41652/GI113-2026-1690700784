e4bfea40dd9115bd17994266f54511bbec5951d2
Thanakrit Intui <138757081+thanakrit41652@users.noreply.github.com>
2026-08-24 13:48:00 +0700

    Update Program.cs

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -8,7 +8,7 @@
             char rank = 'S';
             int level = 7;
             int maxHp = 240;
-            int CurrentHp = 175;
+            int currentHp = 175;
             float attackPower = 42.5f;
             double critMultiplier = 1.75;
             bool isBoss = true;
@@ -17,21 +17,21 @@
             Console.WriteLine($"Name: {bossName}");
             Console.WriteLine($"Rank: {rank}");
             Console.WriteLine($"Level: {level}");
-            Console.WriteLine($"HP: {CurrentHp} / {maxHp}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
             Console.WriteLine($"Attack Power: {attackPower}");
             Console.WriteLine($"Crit Multiplier: {critMultiplier}");
             Console.WriteLine($"Is Boss: {isBoss}");
             Console.WriteLine();
 
-            int hpPercentage = (CurrentHp * 100) / maxHp;
+            int hpPercentage = (currentHp * 100) / maxHp;
             Console.WriteLine($"HP Percent: {hpPercentage}%");
             Console.WriteLine();
             Console.WriteLine("Kirin takes 60 damage!");
-            CurrentHp = CurrentHp - 60;
+            currentHp = currentHp - 60;
             Console.WriteLine();
             Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
-            Console.WriteLine($"HP: {CurrentHp} / {maxHp}");
-            int newhpPercentage = (CurrentHp * 100) / maxHp;
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            int newhpPercentage = (currentHp * 100) / maxHp;
             Console.WriteLine($"HP Percent: {newhpPercentage}%");
         }
     }
